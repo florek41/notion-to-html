10dad6675ec9beb32f3b8f4ea5093a88eb3d54f5
Bartek Florczak <florek41@o2.pl>
2024-05-21 22:00:56 +0200

    vc

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
 
-app.use(logger("dev"));s
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
