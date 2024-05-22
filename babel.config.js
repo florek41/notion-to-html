0b71f1963c7721dcb153a7bbc7cfa69f422835a3
Bartek Florczak <florek41@o2.pl>
2024-05-22 07:53:45 +0200

    bb

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    ["@babel/preset-env", {
+      "targets": {
+        "node": "current"
+      }
+    }]
+  ],
+  plugins: [
+    "@babel/plugin-transform-flow-strip-types",
+    ["@babel/plugin-transform-runtime", {
+      "useESModules": false
+    }]
+  ]
+};
